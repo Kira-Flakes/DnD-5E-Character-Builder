0b4d5bf76ee5093c2b4513431bfb885b087cfcda
Marc <marc5670@nmsu.edu>
2023-10-04 09:51:33 -0600

    Minorly changed input and added current page indicators
    just committing what i have for now

--- main.js
+++ main.js
@@ -3,3 +3,14 @@
 const charSheet = require('./characterSheet.js');
 
 charSheet.setCharacterName(document.getElementById('charnamein'));
+
+// // import { characterSheet } from './characterSheet.js';
+// const character = new characterSheet();
+// console.log('This is my main test');
+
+function submitBackground() {
+    var _name = document.getElementById("_name");
+    localStorage.setItem("_name", _name);
+    console.log(_name + "submitted as name.");
+    
+}
