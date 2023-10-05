2631b66574eedeac0691c79bf189567a624a7562
John_Gilbert <85953852+JohnMGilbert@users.noreply.github.com>
2023-10-05 16:59:57 -0600

    Create characterUtils.js
    Added utility js file for any helper functions we may need.

---
+++ characterUtils.js
@@ -0,0 +1,12 @@
+// Functions that dertmine attributes based on multiple user inputs
+
+// based on tick boxes the user has selected, determines the most likely
+//class the player would like to role-play under.
+// input: bitmap of the 
+import './api.js';
+let api = new API;
+
+function classSelector(preferences) {
+    classes = API(classes,"");
+    console.log(classes)
+}
